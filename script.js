909a9b0aa6021a6997c1b516e273f6aec55dc50f
fares1233 <77900500+fares1233@users.noreply.github.com>
2025-03-28 13:26:59 +0200

    Update script.js

--- script.js
+++ script.js
@@ -121,7 +121,7 @@ class App {
     const allInOne = [];
     this.#workouts.forEach(work => allInOne.push(work.coords));
     var bounds = L.latLngBounds(allInOne);
-    this.#map.fitBounds(bounds, { padding: [20, 50] });
+    this.#map.fitBounds(bounds, { padding: [50, 50] });
   }
 
   _showForm(mapE) {
